b6676565f5a1ee5cc2f10303b54831924da23c2e
SF-Zhou <sfzhou.scut@gmail.com>
2017-02-08 15:03:32 +0800

    added convertion of MATLAB's cell array to Numpy array

--- field/matlab_engine.py
+++ field/matlab_engine.py
@@ -1,4 +1,25 @@
+import numpy as np
 import matlab.engine
+
+
+def n_array(cell_array):
+    return np.array(getattr(cell_array, '_data')).reshape(cell_array.size, order='F')
+
+
+def to_n_array(v):
+    return n_array(v) if getattr(v, '_data', None) else v
+
+
+def transform_output(func):
+    def new_func(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if isinstance(result, list):
+            return [to_n_array(v) for v in result]
+        elif isinstance(result, tuple):
+            return tuple(to_n_array(v) for v in result)
+        else:
+            return to_n_array(result)
+    return new_func
 
 
 class MatlabEngine:
@@ -7,3 +28,16 @@ class MatlabEngine:
             self.engine = matlab.engine.connect_matlab(session_name)
         else:
             self.engine = matlab.engine.start_matlab()
+
+    @property
+    def e(self) -> matlab.engine.MatlabEngine:
+        return self.engine
+
+    # MATLAB function
+    @transform_output
+    def linspace(self, start, end, count):
+        return self.e.linspace(start, end, count)
+
+    @transform_output
+    def magic(self, size: int):
+        return self.e.magic(size)
--- tests/test_matlab_engine.py
+++ tests/test_matlab_engine.py
@@ -1,6 +1,7 @@
 import field
 import warnings
 import unittest
+import numpy as np
 
 
 class MyTestCase(unittest.TestCase):
@@ -20,6 +21,21 @@ class MyTestCase(unittest.TestCase):
         else:
             with warnings.catch_warnings():
                 warnings.warn("Not Found Shared MATLAB Session")
+
+    def test_matlab_function(self):
+        import matlab.engine
+        session_names = matlab.engine.find_matlab()
+        if session_names:
+            matlab = field.MatlabEngine(session_names[0])
+
+            res = matlab.linspace(0.0, 6.28, 10)
+            self.assertTrue(isinstance(res, np.ndarray))
+            self.assertEqual(res.shape, (1, 10))
+
+            res = matlab.magic(4)
+            self.assertEqual(res.shape, (4, 4))
+        else:
+            self.fail("Not Found Shared MATLAB Session")
 
 
 if __name__ == '__main__':
