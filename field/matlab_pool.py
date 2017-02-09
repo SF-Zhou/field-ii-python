6446168a1adb773c9138626ed07b3d6504c2d1a6
SF-Zhou <sfzhou.scut@gmail.com>
2017-02-09 14:17:46 +0800

    improved MATLAB pool return value engines return [a] and [b], pool return [a, b] engines return a and b, pool return [a, b], too

--- field/matlab_pool.py
+++ field/matlab_pool.py
@@ -1,3 +1,5 @@
+import math
+import functools
 import threading
 import matlab.engine
 from . import MatlabEngine
@@ -19,14 +21,22 @@ class MatlabPool:
     def parallel(self, worker_factory, task: list, args: tuple=()):
         thread_pool = []
         worker_pool = []
+        sub_task_count = math.ceil(len(task) / self.engine_count)
         for i, engine in enumerate(self.engines):
-            sub_task = task[i::self.engine_count]
+            sub_task = task[i * sub_task_count:(i + 1) * sub_task_count]
             worker = worker_factory(engine, sub_task)
             worker_pool.append(worker)
 
             thread = threading.Thread(target=worker.start, args=args)
             thread.start()
             thread_pool.append(thread)
+
+        # wait for finishing
         for thread in thread_pool:
             thread.join()
-        return [worker.value for worker in worker_pool]
+
+        # concat when result is list
+        if all(map(lambda w: isinstance(w.value, list), worker_pool)):
+            return functools.reduce(lambda a, b: a + b, (worker.value for worker in worker_pool), [])
+        else:
+            return [worker.value for worker in worker_pool]
