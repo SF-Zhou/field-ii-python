6798bfd9607db409b186596027844f32e90a3b0a
SF-Zhou <sfzhou.scut@gmail.com>
2017-02-13 09:37:03 +0800

    added measure version of execution running without output, improved accuracy

--- cpp_method/method/delay_and_sum.hpp
+++ cpp_method/method/delay_and_sum.hpp
@@ -5,7 +5,11 @@
 void delay_and_sum(float* signals, float* image, const Para& para) {
     int image_idx = 0;
     ff (i, para.line_count) {
-        cerr << "# Current Line: " << i << endl;
+
+#ifndef MEASURE
+        printf("# Current Line: %d\n", i);
+#endif
+
         float *signal_line = signals + i * para.element_count * para.data_length;
         ff (j, para.row_count) {
             float vertical_distance = para.z_start + j * para.pixel_height;
