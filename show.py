8f484f9f5bc9d124d5bac15a092a2ff32d2d32ae
SF-Zhou <sfzhou.scut@gmail.com>
2017-02-13 09:40:05 +0800

    added a simple show tool

---
+++ show.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import viewer
+import simulate
+import numpy as np
+
+
+if len(sys.argv) != 3:
+    raise EnvironmentError("""Run this program like this:
+    python3 show.py configs/multi_scat.json delay_and_sum""")
+
+config_path = sys.argv[1]
+if not os.path.exists(config_path):
+    raise FileNotFoundError("Not Found {}".format(config_path))
+method = sys.argv[2]
+
+para = simulate.Parameter()
+para.load(config_path)
+
+image_path = para.image_path + '.' + method
+
+if not os.path.exists(image_path):
+    raise FileNotFoundError("Not Found {}".format(image_path))
+
+image = np.fromfile(image_path, dtype=np.float32)
+image.shape = para.line_count, para.row_count
+viewer.show(image, para)
